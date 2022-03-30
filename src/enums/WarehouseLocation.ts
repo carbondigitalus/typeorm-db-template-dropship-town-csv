9e2cfbe71cecbc87a6281bccc7bb86030069c2d5
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-03-30 02:24:24 -0400

    Create WarehouseLocation.ts

---
+++ src/enums/WarehouseLocation.ts
@@ -0,0 +1,33 @@
+export class WarehouseLocation {
+    z11788: number;
+    z11729: number;
+    z17112: number;
+    z33513: number;
+    z43526: number;
+    z44050: number;
+    z63026: number;
+    z7726: number;
+    z78723: number;
+    z8721: number;
+    z90640: number;
+    z91350: number;
+    z91789: number;
+    z97202: number;
+
+    constructor() {
+        this.z11788 = 11788;
+        this.z11729 = 11729;
+        this.z17112 = 17112;
+        this.z33513 = 33513;
+        this.z43526 = 43526;
+        this.z44050 = 44050;
+        this.z63026 = 63026;
+        this.z7726 = 7726;
+        this.z78723 = 78723;
+        this.z8721 = 8721;
+        this.z90640 = 90640;
+        this.z91350 = 91350;
+        this.z91789 = 91789;
+        this.z97202 = 97202;
+    }
+}
