69ca0d4b6955233f4dbd7ea09384dc59cb44bb37
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-03-30 02:24:32 -0400

    Delete PriceCheck.ts

--- src/enums/PriceCheck.ts
+++
@@ -1,9 +0,0 @@
-export class PriceCheck {
-    Decimal: RegExp;
-    Whole: RegExp;
-
-    constructor() {
-        this.Decimal = new RegExp('^[0-9]d{0,9}(.d{1,2})?%?$');
-        this.Whole = new RegExp('^d+$');
-    }
-}
