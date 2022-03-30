dfbe5c6d838e015b923ce315d2e696be574c24c3
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-03-30 02:24:26 -0400

    Create WarehouseCode.ts

---
+++ src/enums/WarehouseCode.ts
@@ -0,0 +1,33 @@
+export class WarehouseCode {
+    AIP: string;
+    CWR: string;
+    DDS: string;
+    DFY: string;
+    DNH: string;
+    FGX: string;
+    FWN: string;
+    KHD: string;
+    MEGA: string;
+    MNA: string;
+    MPC: string;
+    TLD: string;
+    TWH: string;
+    XSD: string;
+
+    constructor() {
+        this.AIP = 'AIP';
+        this.CWR = 'CWR';
+        this.DDS = 'DDS';
+        this.DFY = 'DFY';
+        this.DNH = 'DNH';
+        this.FGX = 'FGX';
+        this.FWN = 'FWN';
+        this.KHD = 'KHD';
+        this.MEGA = 'MEGA';
+        this.MNA = 'MNA';
+        this.MPC = 'MPC';
+        this.TLD = 'TLD';
+        this.TWH = 'TWH';
+        this.XSD = 'XSD';
+    }
+}
