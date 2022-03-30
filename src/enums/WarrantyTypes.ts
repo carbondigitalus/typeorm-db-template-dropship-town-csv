49e2343d04239265e6a4dd9a5c92fb525a679ba4
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-03-30 02:24:21 -0400

    Create WarrantyTypes.ts

---
+++ src/enums/WarrantyTypes.ts
@@ -0,0 +1,33 @@
+export class WarrantyTypes {
+    None: string;
+    Lifetime: string;
+    ThirtyDay: string;
+    SixtyDay: string;
+    NinetyDay: string;
+    OneYear: string;
+    TwoYear: string;
+    ThreeYear: string;
+    FourYear: string;
+    FiveYear: string;
+    SevenYear: string;
+    TenYear: string;
+    TwentyYear: string;
+    TwentyFiveYear: string;
+
+    constructor() {
+        this.None = 'No Warranty';
+        this.Lifetime = 'Lifetime Warranty';
+        this.ThirtyDay = '30 Day Warranty';
+        this.SixtyDay = '60 Day Warranty';
+        this.NinetyDay = '90 Day Warranty';
+        this.OneYear = '1 Year Warranty';
+        this.TwoYear = '2 Year Warranty';
+        this.ThreeYear = '3 Year Warranty';
+        this.FourYear = '4 Year Warranty';
+        this.FiveYear = '5 Year Warranty';
+        this.SevenYear = '7 Year Warranty';
+        this.TenYear = '10 Year Warranty';
+        this.TwentyYear = '20 Year Warranty';
+        this.TwentyFiveYear = '25 Year Warranty';
+    }
+}
